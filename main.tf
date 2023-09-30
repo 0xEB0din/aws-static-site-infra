33e58623e2e1a76e023a607adacabbcd5d163c19
ahmedskaya <ahmd.ru@gmail.com>
2023-09-30 21:57:48 +0800

    Terraform: added VPC and ALB configs

--- main.tf
+++ main.tf
@@ -1,55 +1,155 @@
-module "template_files" {
-    source = "hashicorp/dir/template"
-
-    base_dir = "${path.module}/site"
-}
-
 provider "aws" {
-    region = var.aws_region
+  region = var.aws_region
 }
 
-resource "aws_s3_bucket" "hosting_bucket" {
-    bucket = var.bucket_name
-    tags = {
-        "Name"        = "Static Site Hosting Bucket"
-        "Environment" = "Dev"
+# VPC and Subnet config
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16" # Example CIDR block
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "main-vpc"
+  }
+}
+
+resource "aws_subnet" "public_subnet_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"        # CIDR block for the first subnet
+  availability_zone       = "${var.aws_region}a" # Adjust based on your region, e.g., us-west-2a
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-a"
+  }
+}
+
+resource "aws_subnet" "public_subnet_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"        # CIDR block for the second subnet
+  availability_zone       = "${var.aws_region}b" # Adjust based on your region, e.g., us-west-2b
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-b"
+  }
+}
+
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.main.id
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "public_a" {
+  subnet_id      = aws_subnet.public_subnet_a.id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_b" {
+  subnet_id      = aws_subnet.public_subnet_b.id
+  route_table_id = aws_route_table.public.id
+}
+
+# ALB config
+resource "aws_security_group" "alb_sg" {
+  vpc_id = aws_vpc.main.id
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Allow all IPs
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "alb-sg"
+  }
+}
+
+resource "aws_lb" "main" {
+  name               = "demo-alb-staticsite"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets = [
+    aws_subnet.public_subnet_a.id,
+    aws_subnet.public_subnet_b.id
+  ]
+  enable_deletion_protection = false
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type = "redirect"
+    redirect {
+      port        = "80"
+      protocol    = "HTTP"
+      status_code = "HTTP_301"
+      host        = "${var.bucket_name}.s3-website.${var.aws_region}.amazonaws.com"
+      path        = "/#{path}"
     }
+  }
+}
+
+
+
+# S3 Bucket
+resource "aws_s3_bucket" "hosting_bucket" {
+  bucket = var.bucket_name
+  tags = {
+    "Name"        = "Static Site Hosting Bucket"
+    "Environment" = "Dev"
+  }
 }
 
 resource "aws_s3_bucket_policy" "hosting_bucket_policy" {
-    bucket = aws_s3_bucket.hosting_bucket.id
+  bucket = aws_s3_bucket.hosting_bucket.id
 
-    policy = jsonencode({
-        "Version": "2012-10-17",
-        "Statement": [
-            {
-                "Effect": "Allow",
-                "Principal": "*",
-                "Action": "s3:GetObject",
-                "Resource": "arn:aws:s3:::${var.bucket_name}/*"
-            }
-        ]
-    })
+  policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : "*",
+        "Action" : "s3:GetObject",
+        "Resource" : "arn:aws:s3:::${var.bucket_name}/*"
+      }
+    ]
+  })
 }
 
 resource "aws_s3_bucket_website_configuration" "hosting_bucket_website_configuration" {
-    bucket = aws_s3_bucket.hosting_bucket.id
+  bucket = aws_s3_bucket.hosting_bucket.id
 
-    index_document {
-      suffix = "index.html"
-    }
+  index_document {
+    suffix = "index.html"
+  }
 }
 
-resource "aws_s3_object" "hosting_bucket_files" {
-    bucket = aws_s3_bucket.hosting_bucket.id
+resource "aws_s3_bucket_object" "hosting_bucket_files" {
+  for_each = fileset("${path.module}/site", "**/*") # All files in '/site' to be uploaded
 
-    for_each = module.template_files.files
-
-    key = each.key
-    content_type = each.value.content_type
-
-    source  = each.value.source_path
-    content = each.value.content
-
-    etag = each.value.digests.md5
+  bucket = aws_s3_bucket.hosting_bucket.id
+  key    = each.value
+  source = "${path.module}/site/${each.value}"
+  etag   = filemd5("${path.module}/site/${each.value}")
 }
--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,7 @@
-output "website_url" {
-    description = "URL of the website"
-    value = aws_s3_bucket_website_configuration.hosting_bucket_website_configuration.website_endpoint
+output "alb_dns_name" {
+  value = aws_lb.main.dns_name
+}
+
+output "s3_website_url" {
+  value = aws_s3_bucket.hosting_bucket.website_endpoint
 }
